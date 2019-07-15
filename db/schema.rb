f6151e442cf62ac5fab3d56b18f6b8b193ef9bbc
Brady Klein <bradyk@zg03384.zillow.local>
2019-07-15 11:25:10 -0700

    Upgraded db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_15_172607) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -28,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_172607) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
